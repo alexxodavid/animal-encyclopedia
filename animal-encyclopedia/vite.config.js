04ab723b1520c65b7cb12ba8dc46b1dda4fd7ac9
Alex Odysseus David <alexxodavid@gmail.com>
2025-05-21 14:35:27 +0000

    ud

--- animal-encyclopedia/vite.config.js
+++ animal-encyclopedia/vite.config.js
@@ -1,2 +1,10 @@
-import { defineConfig } from 'vite';
-export default defineConfig({});
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '/src': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
