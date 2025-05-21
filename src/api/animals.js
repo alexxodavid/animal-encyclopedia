cd5ea315fbf106897fd633c2f3fb293f8a748c01
Alex Odysseus David <alexxodavid@gmail.com>
2025-05-21 09:51:21 -0400

    Delete src directory

--- src/api/animals.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://api.api-ninjas.com/v1/animals';
-
-export async function fetchRandomAnimals(limit = 10) {
-  const res = await fetch(`${BASE_URL}?limit=${limit}`, {
-    headers: { 'X-Api-Key': import.meta.env.VITE_API_NINJAS_KEY }
-  });
-  if (!res.ok) throw new Error(`API error: ${res.status}`);
-  return res.json();
-}
